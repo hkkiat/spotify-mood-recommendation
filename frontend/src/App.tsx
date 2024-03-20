d155752dc8e84e156ab00537b513574dae95bdba
chinweenie <chinweenie@istox.com>
2024-03-20 23:46:25 +0800

    Add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './css/App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/homepage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </div>
+    //<div className='App'>
+    //  <header className='App-header'>
+    //    <img src={logo} className='App-logo' alt='logo' />
+    //    <p>
+    //      Edit <code>src/App.tsx</code> and save to reload.
+    //    </p>
+    //    <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+    //      Learn React
+    //    </a>
+    //  </header>
+    //</div>
   );
 }
 
---
+++ frontend/src/components/homepage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div>Hello world! this is a react functional component</div>;
+};
+
+export default HomePage;
