9f7f85d9ad47ffac86cc57c0feafa146e74e59c9
Clement <clement7903@hotmail.com>
2024-03-31 15:58:18 +0800

    add custom styling for checkboxes

--- frontend/src/components/moodlog/mostimpact.tsx
+++ frontend/src/components/moodlog/mostimpact.tsx
@@ -7,13 +7,15 @@ This component is used to display the question what has the most impact on you t
 
 function MostImpact() {
     return (
-        <div>
+        <div className={styles.checkboxContainer}>
             <label className={`form-label mt-2 ${styles.moodlogquestionheader}`}>What has the most impact on you today?</label>
-            <Checkbox id="option1Checkbox" label="Family" value="Family" />
-            <Checkbox id="option2Checkbox" label="Friends" value="Friends" />
-            <Checkbox id="option3Checkbox" label="Work" value="Work" />
-            <Checkbox id="option4Checkbox" label="Life" value="Life" />
-            <Checkbox id="option5Checkbox" label="Study" value="Study" />
+            <div className={styles.checkboxGroup}>
+                <Checkbox id="option1Checkbox" label="Family" value="Family" />
+                <Checkbox id="option2Checkbox" label="Friends" value="Friends" />
+                <Checkbox id="option3Checkbox" label="Work" value="Work" />
+                <Checkbox id="option4Checkbox" label="Life" value="Life" />
+                <Checkbox id="option5Checkbox" label="Study" value="Study" />
+            </div>
         </div>
     );
 }
