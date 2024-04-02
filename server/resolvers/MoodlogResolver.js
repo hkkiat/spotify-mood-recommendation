029cabfd2ef0d6cdcb48de52d637c48dd01f2a4b
Clement <clement7903@hotmail.com>
2024-04-03 00:32:42 +0800

    add getAllMoodLogs, createMoodLog to backend

--- server/resolvers/MoodlogResolver.js
+++ server/resolvers/MoodlogResolver.js
@@ -1,14 +1,38 @@
 // THIS FILE CONTAINS THE QUERIES & MUTATIONS FOR MOOD LOGGING FEATURE
 
-async function getOverallFeeling(_, args, { db })
-{
-  /*Q2: Write code to talk to DB and read the list of travellers
-   * */
-//   result = await db.collection('travellers').find().toArray()
-//   console.log(result)
-//   return result
+async function getAllMoodLogs(_, { email }, { db }) {
+  /*
+  Code to extract
+  */
+
+  try {
+    console.log("Extracting all mood logs for... ", email)
+    // Assuming db.collection('moodlog') retrieves mood logs from your database
+    const moodLogs = await db.collection('moodlog').find({ email: email }).toArray();
+    return moodLogs;
+  } catch (error) {
+    console.error('Error fetching mood logs:', error);
+    throw new Error('Failed to fetch mood logs');
+  }
 
   /*End of Q2*/
 }
 
-module.exports = {getOverallFeeling};
+async function createMoodLog(_, { moodlog }, { db }) {
+  try {
+    console.log(moodlog)
+    const collection = db.collection('moodlog');
+
+    // Insert the moodlog into the database
+    const result = await collection.insertOne(moodlog);
+
+    // Retrieve the inserted moodlog from the database
+    const insertedMoodLog = await collection.findOne({ _id: result.insertedId });
+
+    return insertedMoodLog;
+  } catch (error) {
+    throw new Error('Failed to create mood log: ' + error.message);
+  }
+}
+
+module.exports = { getAllMoodLogs, createMoodLog };
