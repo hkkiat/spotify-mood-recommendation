7868ffe973ff070486d04707725abb8efab08c65
chinweenie <chinweenie@istox.com>
2024-05-03 00:38:08 +0800

    Add redirect if status code is 403

--- frontend/src/Client.ts
+++ frontend/src/Client.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink, defaultDataIdFromObject } from 'apollo-boost';
 import { ApolloLink } from 'apollo-link';
 import * as configs from './configs';
+import { onError } from 'apollo-link-error';
+
 
 import { createHttpLink } from 'apollo-link-http';
 
@@ -16,6 +18,23 @@ const cache = new InMemoryCache({
   },
 });
 
+const ErrorLink = onError(({ graphQLErrors, networkError, response, operation }) => {
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+
+    let networkErrorObj = networkError as any;
+    const statusCode = networkErrorObj.statusCode;
+
+    if (statusCode === 403) {
+      console.log("Invalid session, redirecting to login page...");
+      alert("Invalid session, redirecting to login page...");
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 1500);
+    }
+  }
+})
+
 const defaultOptions: any = {
   watchQuery: {
     fetchPolicy: 'cache-and-network',
@@ -24,11 +43,11 @@ const defaultOptions: any = {
   query: {
     fetchPolicy: 'cache-and-network',
     errorPolicy: 'all',
-  },
+  }
 };
 
 export const defaultClient = new ApolloClient({
   cache,
   defaultOptions,
-  link: httpLink,
+  link: ApolloLink.from([ErrorLink, httpLink]),
 });
